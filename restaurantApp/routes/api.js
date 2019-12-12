669724c0caebe8548ef7cc96715e46566a61941a
Angelo Capozzi <acap613@gmail.com>
2019-12-11 21:18:37 -0500

    commiting new additions to new angelo branch

--- restaurantApp/routes/api.js
+++ restaurantApp/routes/api.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET apis listing. */
+router.get('/tables', function(req, res, next) {
+  res.send('respond with some tables');
+});
+router.post('/reserve', function(req, res, next) {
+  res.send('respond with a confirmation');
 });
 
 module.exports = router;
